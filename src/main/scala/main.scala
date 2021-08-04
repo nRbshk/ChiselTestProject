50ef524754a787326ba5a9798418ad18cfe88c6d
Рябощук Никита Михайлович <nryaboshchuk@elvees.com>
2021-08-04 20:04:34 +0300

    Add new file main.scala

---
+++ src/main/scala/main.scala
@@ -0,0 +1,6 @@
+import chisel3.stage.ChiselStage
+import fifo._
+
+object Test extends App {
+    (new ChiselStage).emitVerilog(new Fifo_sp(32, 32), Array("-td", "srcv/fifo", "--emit-modules", "verilog"))
+}
