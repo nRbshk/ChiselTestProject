1932fcf2f5e9834751d31908b4662fba783634e0
nRbshk <n.ryaboshchuk@gmail.com>
2021-08-05 15:37:21 +0300

    re init repo

--- src/main/scala/main.scala
+++
@@ -1,6 +0,0 @@
-import chisel3.stage.ChiselStage
-import fifo._
-
-object Test extends App {
-    (new ChiselStage).emitVerilog(new Fifo(32, 32), Array("-td", "srcv/fifo", "--emit-modules", "verilog"))
-}
